a6ef4d874c8f8c50718312a57b3454799c5fa91e
Ken Ijiwoye <ken@kenijiwoye.com>
2016-05-09 23:25:01 -0400

    fix issues with gkeditor in production

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module Port1
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+    config.assets.precompile += Ckeditor.assets
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
